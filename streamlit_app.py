d779e035b55171623cc108b2dde7475648568833
Ghislain LEVEQUE <ghislain@leveque.cc>
2024-12-06 21:49:03 +0000

    Aide pour afficher une liste

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,14 @@
 import streamlit as st
 import random
 from pokedex import POKEDEX
+from outils import affiche_liste
 
-def afficher_liste():
+def afficher_tous_les_pokemons():
+    descriptions = []
     for pokemon in POKEDEX:
-        st.write(pokemon.decris_toi())
+        descriptions.append(pokemon.decris_toi())
+        
+    affiche_liste(descriptions)
 
 def afficher_un_au_hasard():
     pokemon = random.choice (POKEDEX)
@@ -14,6 +18,6 @@ def afficher_un_au_hasard():
 def page_principale():
     st.title("👾 Mon pokédex")
     st.write("Mon premier pokédex")
-    afficher_liste()
+    afficher_tous_les_pokemons()
 
 page_principale()
