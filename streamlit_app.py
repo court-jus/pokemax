8b27205117b5e772460a5b3f185fdaa74f750052
Max1lev <maximelev31@gmail.com>
2024-12-06 21:38:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("👾 My new app")
+st.title("👾 Mon pokédex")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Mon premier pokédex"
 )
